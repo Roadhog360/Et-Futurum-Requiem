f2d7c6ae75f48ca872ebc7b2895e350cdec0da1d
Pilzinsel64 <pilzinsel64@gmx.de>
2024-02-13 11:32:29 +0100

    add comments to client event handler

--- src/main/java/ganymedes01/etfuturum/backhand/BackhandClientEventHandler.java
+++ src/main/java/ganymedes01/etfuturum/backhand/BackhandClientEventHandler.java
@@ -80,6 +80,12 @@ public class BackhandClientEventHandler {
     //     Minecraft.getMinecraft().leftClickCounter = i;
     // }
 
+    /*
+     * ---------------------------
+     * K E Y B O A R D   I N P U T
+     * ---------------------------
+     */
+
     @SideOnly(Side.CLIENT)
     @SubscribeEvent
     public void onKeyInputEvent(InputEvent.KeyInputEvent event) {
@@ -98,6 +104,12 @@ public class BackhandClientEventHandler {
         }
     }
 
+    /*
+     * -------------------------------
+     * I N V E N T O R Y   T W E A K S
+     * -------------------------------
+     */
+
     @SideOnly(Side.CLIENT)
     @Method(modid="inventorytweaks")
     public void invTweaksSwapPatch() {
@@ -111,8 +123,9 @@ public class BackhandClientEventHandler {
     }
 
     @SideOnly(Side.CLIENT)
+    @Method(modid="inventorytweaks")
     @SubscribeEvent
-    public void onClientTick(TickEvent.ClientTickEvent event) {
+    public void invTweaks_OnClientTick(TickEvent.ClientTickEvent event) {
         if (invTweaksDelay > 0) {
             invTweaksDelay--;
             if (invTweaksDelay == 0) {
@@ -130,6 +143,12 @@ public class BackhandClientEventHandler {
         InvTweaks.getConfigManager().getConfig().setProperty("enableAutoRefill",String.valueOf(prevInvTweaksAutoRefill));
         InvTweaks.getConfigManager().getConfig().setProperty("autoRefillBeforeBreak",String.valueOf(prevInvTweaksBreakRefill));
     }
+
+    /*
+     * -------------------------
+     * C L I E N T   H E L P E R
+     * -------------------------
+     */
 
     @SideOnly(Side.CLIENT)
     @SubscribeEvent
@@ -184,126 +203,147 @@ public class BackhandClientEventHandler {
         //     }
         // }
     }
+
+    /* 
+     * -------------------------
+     * R E N D E R   H O T B A R
+     * -------------------------
+     */
  
-     @SubscribeEvent
-     public void renderHotbarOverlay(RenderGameOverlayEvent event) {
-         if (event.type == RenderGameOverlayEvent.ElementType.HOTBAR) {
-             Minecraft mc = Minecraft.getMinecraft();
-             renderHotbar(mc.ingameGUI, event.resolution.getScaledWidth(), event.resolution.getScaledHeight(), event.partialTicks);
-         }
-     }
- 
-     protected void renderHotbar(GuiIngame gui, int width, int height, float partialTicks) {
-         Minecraft mc = Minecraft.getMinecraft();
-         ItemStack itemstack = Backhand.INSTANCE.getOffhandItem(mc.thePlayer);
-         if (itemstack == null) {
-             return;
-         }
- 
-         mc.mcProfiler.startSection("actionBar");
- 
-         GL11.glEnable(GL11.GL_BLEND);
-         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-         mc.renderEngine.bindTexture(new ResourceLocation("textures/gui/widgets.png"));
- 
-         gui.drawTexturedModalRect(width / 2 - 125, height - 22, 0, 0, 11, 22);
-         gui.drawTexturedModalRect(width / 2 - 125 + 11, height - 22, 182 - 11, 0, 11, 22);
- 
-         GL11.glDisable(GL11.GL_BLEND);
-         GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-         RenderHelper.enableGUIStandardItemLighting();
- 
-         int x = width / 2 - 122;
-         int z = height - 16 - 3;
-         renderOffhandInventorySlot(x, z, partialTicks);
- 
-         RenderHelper.disableStandardItemLighting();
-         GL11.glDisable(GL12.GL_RESCALE_NORMAL);
- 
-         mc.mcProfiler.endSection();
-     }
- 
-     protected void renderOffhandInventorySlot(int p_73832_2_, int p_73832_3_, float p_73832_4_) {
-         Minecraft mc = Minecraft.getMinecraft();
-         ItemStack itemstack = Backhand.INSTANCE.getOffhandItem(mc.thePlayer);
- 
-         if (itemstack != null)
-         {
-             float f1 = itemstack.animationsToGo - p_73832_4_;
- 
-             if (f1 > 0.0F)
-             {
-                 GL11.glPushMatrix();
-                 float f2 = 1.0F + f1 / 5.0F;
-                 GL11.glTranslatef(p_73832_2_ + 8, p_73832_3_ + 12, 0.0F);
-                 GL11.glScalef(1.0F / f2, (f2 + 1.0F) / 2.0F, 1.0F);
-                 GL11.glTranslatef((-(p_73832_2_ + 8)), (-(p_73832_3_ + 12)), 0.0F);
-             }
- 
-             RenderItem.getInstance().renderItemAndEffectIntoGUI(mc.fontRenderer, mc.getTextureManager(), itemstack, p_73832_2_, p_73832_3_);
- 
-             if (f1 > 0.0F)
-             {
-                 GL11.glPopMatrix();
-             }
- 
-             RenderItem.getInstance().renderItemOverlayIntoGUI(mc.fontRenderer, mc.getTextureManager(), itemstack, p_73832_2_, p_73832_3_);
-         }
-     }
- 
-     public static int renderPass;
-     @SubscribeEvent
-     public void onRenderHand(RenderHandEvent event) {
-         renderPass = event.renderPass;
-     }
- 
-     /**
-      * Bend the models when the item in left hand is used
-      * And stop the right hand inappropriate bending
-      */
-     @SubscribeEvent(priority = EventPriority.LOW)
-     public void renderPlayerLeftItemUsage(RenderLivingEvent.Pre event){
-         if(event.entity instanceof EntityPlayer) {
-             EntityPlayer entityPlayer = (EntityPlayer) event.entity;
-             renderingPlayer = entityPlayer;
-             ItemStack offhand = Backhand.INSTANCE.getOffhandItem(entityPlayer);
-             if (offhand != null && event.renderer instanceof RenderPlayer) {
-                 RenderPlayer renderer = ((RenderPlayer) event.renderer);
-                 renderer.modelArmorChestplate.heldItemLeft = renderer.modelArmor.heldItemLeft = renderer.modelBipedMain.heldItemLeft = 1;
-                 if (entityPlayer.getItemInUseCount() > 0 && entityPlayer.getItemInUse() == offhand) {
-                     EnumAction enumaction = offhand.getItemUseAction();
-                     if (enumaction == EnumAction.block) {
-                         renderer.modelArmorChestplate.heldItemLeft = renderer.modelArmor.heldItemLeft = renderer.modelBipedMain.heldItemLeft = 3;
-                     } else if (enumaction == EnumAction.bow) {
-                         renderer.modelArmorChestplate.aimedBow = renderer.modelArmor.aimedBow = renderer.modelBipedMain.aimedBow = true;
-                     }
-                     ItemStack mainhand = entityPlayer.inventory.getCurrentItem();
-                     renderer.modelArmorChestplate.heldItemRight = renderer.modelArmor.heldItemRight = renderer.modelBipedMain.heldItemRight = mainhand != null ? 1 : 0;
-                 }
-             }
-         }
-     }
- 
-     /**
-      * Reset models to default values
-      */
-     @SubscribeEvent(priority = EventPriority.LOW)
-     public void resetPlayerLeftHand(RenderPlayerEvent.Post event){
-         event.renderer.modelArmorChestplate.heldItemLeft = event.renderer.modelArmor.heldItemLeft = event.renderer.modelBipedMain.heldItemLeft = 0;
-     }
- 
-     @SubscribeEvent
-     public void render3rdPersonOffhand(RenderPlayerEvent.Specials.Post event) {
-         if (!ConfigFunctions.offhand.allowEmptyOffhand && Backhand.INSTANCE.getOffhandItem(event.entityPlayer) == null) {
-             return;
-         }
- 
-         GL11.glPushMatrix();
-         ModelBiped biped = (ModelBiped) event.renderer.modelBipedMain;
-         RenderOffhandPlayer.itemRenderer.updateEquippedItem();
-         renderOffhandPlayer.updateFovModifierHand();
-         RenderOffhandPlayer.itemRenderer.renderOffhandItemIn3rdPerson(event.entityPlayer, biped, event.partialRenderTick);
-         GL11.glPopMatrix();
-     }
+    /*
+     * Extends the players hotbar by rendering an overlay.
+     */
+    @SubscribeEvent
+    public void renderHotbarOverlay(RenderGameOverlayEvent event) {
+        if (event.type == RenderGameOverlayEvent.ElementType.HOTBAR) {
+            Minecraft mc = Minecraft.getMinecraft();
+            renderHotbar(mc.ingameGUI, event.resolution.getScaledWidth(), event.resolution.getScaledHeight(), event.partialTicks);
+        }
+    }
+
+    /*
+     * Renders the offhand hotbar.
+     */
+    protected void renderHotbar(GuiIngame gui, int width, int height, float partialTicks) {
+        Minecraft mc = Minecraft.getMinecraft();
+        ItemStack itemstack = Backhand.INSTANCE.getOffhandItem(mc.thePlayer);
+        if (itemstack == null) {
+            return;
+        }
+
+        mc.mcProfiler.startSection("actionBar");
+
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        mc.renderEngine.bindTexture(new ResourceLocation("textures/gui/widgets.png"));
+
+        gui.drawTexturedModalRect(width / 2 - 125, height - 22, 0, 0, 11, 22);
+        gui.drawTexturedModalRect(width / 2 - 125 + 11, height - 22, 182 - 11, 0, 11, 22);
+
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+        RenderHelper.enableGUIStandardItemLighting();
+
+        int x = width / 2 - 122;
+        int z = height - 16 - 3;
+        renderOffhandInventorySlot(x, z, partialTicks);
+
+        RenderHelper.disableStandardItemLighting();
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+
+        mc.mcProfiler.endSection();
+    }
+
+    /*
+     * Renders the offhand inventory slot on the left side of the hotbar.
+     */
+    protected void renderOffhandInventorySlot(int p_73832_2_, int p_73832_3_, float p_73832_4_) {
+        Minecraft mc = Minecraft.getMinecraft();
+        ItemStack itemstack = Backhand.INSTANCE.getOffhandItem(mc.thePlayer);
+
+        if (itemstack != null)
+        {
+            float f1 = itemstack.animationsToGo - p_73832_4_;
+
+            if (f1 > 0.0F)
+            {
+                GL11.glPushMatrix();
+                float f2 = 1.0F + f1 / 5.0F;
+                GL11.glTranslatef(p_73832_2_ + 8, p_73832_3_ + 12, 0.0F);
+                GL11.glScalef(1.0F / f2, (f2 + 1.0F) / 2.0F, 1.0F);
+                GL11.glTranslatef((-(p_73832_2_ + 8)), (-(p_73832_3_ + 12)), 0.0F);
+            }
+
+            RenderItem.getInstance().renderItemAndEffectIntoGUI(mc.fontRenderer, mc.getTextureManager(), itemstack, p_73832_2_, p_73832_3_);
+
+            if (f1 > 0.0F)
+            {
+                GL11.glPopMatrix();
+            }
+
+            RenderItem.getInstance().renderItemOverlayIntoGUI(mc.fontRenderer, mc.getTextureManager(), itemstack, p_73832_2_, p_73832_3_);
+        }
+    }
+
+    public static int renderPass;
+    @SubscribeEvent
+    public void onRenderHand(RenderHandEvent event) {
+        renderPass = event.renderPass;
+    }
+
+    /* 
+     * -------------------------
+     * R E N D E R   P L A Y E R
+     * -------------------------
+     */
+
+    /**
+     * Bend the models when the item in left hand is used
+     * And stop the right hand inappropriate bending
+     */
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void renderPlayerLeftItemUsage(RenderLivingEvent.Pre event){
+        if(event.entity instanceof EntityPlayer) {
+            EntityPlayer entityPlayer = (EntityPlayer) event.entity;
+            renderingPlayer = entityPlayer;
+            ItemStack offhand = Backhand.INSTANCE.getOffhandItem(entityPlayer);
+            if (offhand != null && event.renderer instanceof RenderPlayer) {
+                RenderPlayer renderer = ((RenderPlayer) event.renderer);
+                renderer.modelArmorChestplate.heldItemLeft = renderer.modelArmor.heldItemLeft = renderer.modelBipedMain.heldItemLeft = 1;
+                if (entityPlayer.getItemInUseCount() > 0 && entityPlayer.getItemInUse() == offhand) {
+                    EnumAction enumaction = offhand.getItemUseAction();
+                    if (enumaction == EnumAction.block) {
+                        renderer.modelArmorChestplate.heldItemLeft = renderer.modelArmor.heldItemLeft = renderer.modelBipedMain.heldItemLeft = 3;
+                    } else if (enumaction == EnumAction.bow) {
+                        renderer.modelArmorChestplate.aimedBow = renderer.modelArmor.aimedBow = renderer.modelBipedMain.aimedBow = true;
+                    }
+                    ItemStack mainhand = entityPlayer.inventory.getCurrentItem();
+                    renderer.modelArmorChestplate.heldItemRight = renderer.modelArmor.heldItemRight = renderer.modelBipedMain.heldItemRight = mainhand != null ? 1 : 0;
+                }
+            }
+        }
+    }
+
+    /**
+     * Reset models to default values
+     */
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void resetPlayerLeftHand(RenderPlayerEvent.Post event){
+        event.renderer.modelArmorChestplate.heldItemLeft = event.renderer.modelArmor.heldItemLeft = event.renderer.modelBipedMain.heldItemLeft = 0;
+    }
+
+    @SubscribeEvent
+    public void render3rdPersonOffhand(RenderPlayerEvent.Specials.Post event) {
+        if (!ConfigFunctions.offhand.allowEmptyOffhand && Backhand.INSTANCE.getOffhandItem(event.entityPlayer) == null) {
+            return;
+        }
+
+        GL11.glPushMatrix();
+        ModelBiped biped = (ModelBiped) event.renderer.modelBipedMain;
+        RenderOffhandPlayer.itemRenderer.updateEquippedItem();
+        renderOffhandPlayer.updateFovModifierHand();
+        RenderOffhandPlayer.itemRenderer.renderOffhandItemIn3rdPerson(event.entityPlayer, biped, event.partialRenderTick);
+        GL11.glPopMatrix();
+    }
 }
