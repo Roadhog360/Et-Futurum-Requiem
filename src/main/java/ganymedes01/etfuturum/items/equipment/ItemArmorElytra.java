95deb6d86b969ad43245468fb57725b438ea25ee
Caedis <Caedis@users.noreply.github.com>
2026-03-13 18:52:12 -0500

    Remove baubles api package (#43)

--- src/main/java/ganymedes01/etfuturum/items/equipment/ItemArmorElytra.java
+++ src/main/java/ganymedes01/etfuturum/items/equipment/ItemArmorElytra.java
@@ -3,6 +3,7 @@ package ganymedes01.etfuturum.items.equipment;
 import baubles.api.BaubleType;
 import baubles.api.expanded.BaubleItemHelper;
 import baubles.api.expanded.IBaubleExpanded;
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Optional;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -24,7 +25,7 @@ import net.minecraft.world.World;
 
 import java.util.List;
 
-@Optional.Interface(iface = "baubles.api.expanded.IBaubleExpanded", modid = "Baubles|Expanded")
+@Optional.Interface(modid = "Baubles|Expanded", iface = "baubles.api.expanded.IBaubleExpanded")
 public class ItemArmorElytra extends BaseItem implements IBaubleExpanded {
 
 	private IIcon broken;
@@ -122,33 +123,40 @@ public class ItemArmorElytra extends BaseItem implements IBaubleExpanded {
 		}
 	}
 
+	@Optional.Method(modid = "Baubles|Expanded")
 	@Override
 	public BaubleType getBaubleType(ItemStack itemstack) {
 		return null;
 	}
 
+	@Optional.Method(modid = "Baubles|Expanded")
 	@Override
 	public void onWornTick(ItemStack itemstack, EntityLivingBase player) {
 	}
 
+	@Optional.Method(modid = "Baubles|Expanded")
 	@Override
 	public void onEquipped(ItemStack itemstack, EntityLivingBase player) {
 	}
 
+	@Optional.Method(modid = "Baubles|Expanded")
 	@Override
 	public void onUnequipped(ItemStack itemstack, EntityLivingBase player) {
 	}
 
+	@Optional.Method(modid = "Baubles|Expanded")
 	@Override
 	public boolean canEquip(ItemStack itemstack, EntityLivingBase player) {
 		return getElytra(player) == null;
 	}
 
+	@Optional.Method(modid = "Baubles|Expanded")
 	@Override
 	public boolean canUnequip(ItemStack itemstack, EntityLivingBase player) {
 		return true;
 	}
 
+	@Optional.Method(modid = "Baubles|Expanded")
 	@Override
 	public String[] getBaubleTypes(ItemStack itemstack) {
 		if (ConfigModCompat.elytraBaublesExpandedCompat == 0) {
