ea8b760ebecd3f7dd6ed71a62072e5185a2a5652
Regiar3dev <regiar3dev@gmail.com>
2026-08-15 02:28:58 -0300

    fixed java.util wildcard import

--- src/main/java/ganymedes01/etfuturum/configuration/ConfigBase.java
+++ src/main/java/ganymedes01/etfuturum/configuration/ConfigBase.java
@@ -12,7 +12,11 @@ import net.minecraftforge.common.config.Property;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Locale;
 
 public abstract class ConfigBase extends Configuration {
 	protected final List<ConfigCategory> configCats = new ArrayList<>();
